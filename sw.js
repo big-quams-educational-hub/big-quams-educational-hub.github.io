78ada156eba46ec6c7da879e6bd2190ab7cc5b7d
Big Quams <bigquamsmedia024@gmail.com>
2026-04-01 18:14:40 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'bqm-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/newsroom.html',
+  '/elibrary.html',
+  '/dyk.html',
+  '/spotlight.html',
+  '/news.json',
+  '/books.json',
+  '/dyk.json',
+  '/spotlight.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).catch(() => {}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
+
